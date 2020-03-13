3be8d500c776bea76a9c47b7931f3f01205f09d1
B-Dunn13 <bcdunn2@asu.edu>
2020-03-12 20:29:48 -0700

    Made the ball_tracking file compatible with the oCam and changed the bounding box in simple_code to rotate with a rectangular shape.

--- ball_tracking.py
+++ ball_tracking.py
@@ -6,6 +6,49 @@ import argparse
 import cv2
 import imutils
 import time
+import liboCams
+
+# find the oCam
+devpath = liboCams.FindCamera('oCam')
+if devpath is None:
+  exit()
+
+test = liboCams.oCams(devpath, verbose = 1)
+
+# get the format list for the oCam
+print 'Format List'
+fmtlist = test.GetFormatList()
+for fmt in fmtlist:
+  print '\t', fmt
+
+# set the format for the oCam (8-bit Bayer GRGR/BGBG,
+# 640L, 480L, 30)
+print 'SET', 8, fmtlist[8]
+test.Set(fmtlist[8])
+
+# get the current camera parameters and set new
+# camera prameters before recording
+print 'Control List'
+ctrlist = test.GetControlList()
+for key in ctrlist:
+	print '\t', key, '\tID:', ctrlist[key]
+	print test.GetControl(ctrlist[key])
+	if key == "White Balance Red Component":
+		test.SetControl(ctrlist[key], 104)
+	elif key == "White Balance Blue Component":
+		test.SetControl(ctrlist[key], 250)
+	elif key == "Gain":
+		test.SetControl(ctrlist[key], 64)
+	elif key == "Exposure (Absolute)":
+		test.SetControl(ctrlist[key], 128)
+	elif key == "Exposure, Auto":
+		test.SetControl(ctrlist[key], 3)
+
+test.Start()
+
+start_time = time.time()
+
+frame_cnt = 0
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -18,45 +61,59 @@ args = vars(ap.parse_args())
 # define the lower and upper boundaries of the "red"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-redLower = (0, 50, 50)
-redUpper = (10, 255, 255)
+# redLower = (0, 50, 50)
+# redUpper = (10, 255, 255)
+GreenLower = (29, 86, 6)
+GreenUpper = (64, 255, 255)
 pts = deque(maxlen=args["buffer"])
 
-# if a video path was not supplied, grab the reference
-# to the webcam
-if not args.get("video", False):
-	vs = VideoStream(src=0).start()
+# # if a video path was not supplied, grab the reference
+# # to the webcam
+# if not args.get("video", False):
+# 	vs = VideoStream(src=0).start()
 
-# otherwise, grab a reference to the video file
-else:
-	vs = cv2.VideoCapture(args["video"])
+# # otherwise, grab a reference to the video file
+# else:
+# 	vs = cv2.VideoCapture(args["video"])
 
-# allow the camera or video file to warm up
-# time.sleep(2.0)
+# # allow the camera or video file to warm up
+# # time.sleep(2.0)
 
 # keep looping
 while True:
 	# grab the current frame
-	frame = vs.read()
-
-	# handle the frame from VideoCapture or VideoStream
-	frame = frame[1] if args.get("video", False) else frame
+	frame = test.GetFrame()
+	BGR = cv2.cvtColor(frame, cv2.COLOR_BAYER_GB2BGR)
+	src = BGR 
 	
-	# if we are viewing a video and we did not grab a frame,
-	# then we have reached the end of the video
-	if frame is None:
-		break
+	# Check if image is loaded fine
+	if src is None:
+		print ('Error opening image!')
+		print ('Usage: simple_code.py [oCam -- default ' + default_file + '] \n')
+		exit()
+	
+	# frame = vs.read()
+
+	# # handle the frame from VideoCapture or VideoStream
+	# frame = frame[1] if args.get("video", False) else frame
+	
+	# # if we are viewing a video and we did not grab a frame,
+	# # then we have reached the end of the video
+	# if frame is None:
+	# 	break
 	
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
-	frame = imutils.resize(frame, width=600)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	# src = imutils.resize(src, width=600)
+	blurred = cv2.medianBlur(src, 3)
+	blurred = cv2.GaussianBlur(blurred, (11, 11), 0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 	
 	# construct a mask for the color "red", then perform
 	# a series of dilations and erosions to remove any small
 	# blobs left in the mask
-	mask = cv2.inRange(hsv, redLower, redUpper)
+	mask = cv2.inRange(hsv, GreenLower, GreenUpper)
+	# mask = cv2.inRange(hsv, redLower, redUpper)
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2)
 
@@ -101,20 +158,22 @@ while True:
 		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 	
 	# show the frame to our screen
-	cv2.imshow("Frame", frame)
+	cv2.imshow("Frame", mask)
+	print 'Result Frame Per Second:', frame_cnt / (time.time() - start_time)
 	key = cv2.waitKey(1) & 0xFF
 	
 	# if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
 		break
+	frame_cnt += 1
 
-# if we are not using a video file, stop the camera video stream
-if not args.get("video", False):
-	vs.stop()
+# # if we are not using a video file, stop the camera video stream
+# if not args.get("video", False):
+# 	vs.stop()
 
-# otherwise, release the camera
-else:
-	vs.release()
+# # otherwise, release the camera
+# else:
+# 	vs.release()
 
 # close all windows
 cv2.destroyAllWindows()
