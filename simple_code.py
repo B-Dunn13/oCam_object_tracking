cf7bf34e4be9114a9b4b1ea6e41a3d91f2973a6d
B-Dunn13 <bcdunn2@asu.edu>
2020-03-13 23:57:13 -0700

    Added more filtering for better detection.

--- simple_code.py
+++ simple_code.py
@@ -19,7 +19,7 @@ for fmt in fmtlist:
   print '\t', fmt
 
 # set the format for the oCam (8-bit Bayer GRGR/BGBG,
-# 640L, 480L, 30)
+# 640L, 480L, 60)
 print 'SET', 8, fmtlist[8]
 test.Set(fmtlist[8])
 
@@ -48,13 +48,6 @@ start_time = time.time()
 frame_cnt = 0
 
 #----------------------------------OBJECT DETECTION----------------------------------
-# define the lower and upper boundaries of the "red"
-# ball in the HSV color space, then initialize the
-# list of tracked points. Must start before the while
-# loop for the previous centroid pts to show.
-GreenLower = (29, 86, 6)
-GreenUpper = (64, 255, 255)
-
 while True:
 	frame = test.GetFrame()
 	BGR = cv.cvtColor(frame, cv.COLOR_BAYER_GB2BGR)
@@ -66,10 +59,16 @@ while True:
 		print ('Usage: simple_code.py [oCam -- default ' + default_file + '] \n')
 		exit()
 	
+	# define the lower and upper boundaries of the "green"
+	# ball in the HSV color space
+	GreenLower = (29, 86, 6)
+	GreenUpper = (64, 255, 255)
+
 	# blur the frame and convert it to the HSV color space
 	src = imutils.resize(src, width=480)
-	blur = cv.medianBlur(src, 3)
-	blur = cv.GaussianBlur(blur, (11, 11), 0)
+	blur = cv.medianBlur(src, 1)
+	blur = cv.GaussianBlur(src, (9, 9), 0)
+	blur = cv.bilateralFilter(blur, 5, 100, 50)
 	hsv = cv.cvtColor(blur, cv.COLOR_BGR2HSV)
 
 	# # construct a mask for the color "red", Lower mask (0-10)
@@ -90,52 +89,73 @@ while True:
 	mask = cv.erode(mask, None, iterations = 2)
 	mask = cv.dilate(mask, None, iterations = 2)
 
-#----------------------------------OBJECT TRACKING----------------------------------
 	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
-	cnts = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
+	contours = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+	contours = imutils.grab_contours(contours)
 	center = None
 
 	# only proceed if at least one contour was found
-	if len(cnts) > 0:
+	if len(contours) > 0:
 		# # find the largest contour in the mask, then use it to
 		# # compute the minimum enclosing circle and centroid
-		# c = max(cnts, key = cv.contourArea)
+		# c = max(contours, key = cv.contourArea)
 		# ((x, y), radius) = cv.minEnclosingCircle(c)
 		
 		# find the largest contour in the mask, then use it to
-		# compute the bounding rectangle and centroid
-		c = max(cnts, key = cv.contourArea)		
+		# compute the minimum area rectangle and centroid
+		c = max(contours, key = cv.contourArea)
 		
-		# use this method when tracking spheres or balls,
-		# bounding box will not rotate
+		# contour approximation
+		epsilon = 0.005*cv.arcLength(c, True)
+		approx = cv.approxPolyDP(c, epsilon, True)
+
+		
+		# use this method when tracking a sphere or ball,
+		# the bounding box will not rotate
 		# x, y, w, h = cv.boundingRect(c)
 		
 		M = cv.moments(c)
 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+		# use this method when tracking rectangular objects,
+		# the bounding box will rotate
+		points = cv.minAreaRect(c)
+		box = cv.boxPoints(points)
+		box = np.int0(box)
+
+		# draw the bounding box and centroid of the object
+		cv.drawContours(src, [approx], 0, (0, 255, 255), 1)
+		cv.drawContours(src, [box], 0, (0, 0, 255), 1)
+		cv.circle(src, center, 5, (0, 0, 255), -1)
+		print "Bounding Box (BL-BR CW):", box
 		
+		# use this method when tracking a sphere or ball,
+		# the bounding box will not rotate
+		# cv.rectangle(src, (x, y), (x + w, y + h), (255, 255, 255), 2)
+		# cv.circle(src, center, 5, (0, 0, 255), -1)
+		# print "Bounding Box:", (x, y), ((x + w), y), ((x + w), (y + h)), (x, (y + h))
+
 		# # only proceed if the radius meets a minimum size
 		# if radius > 10:
 		# 	# draw the bounding box and centroid on the frame
 		# 	cv.circle(src, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-		# 	# cv.rectangle(src, (x, y), (x + w, y + h), (255, 255, 255), 2)
 		# 	cv.circle(src, center, 5, (0, 0, 255), -1)
 
-		# use this method when tracking rectangular objects,
-			# bounding box will rotate
-		rect = cv.minAreaRect(c)
-		box = cv.boxPoints(rect)
-		box = np.int0(box)
-		src = cv.drawContours(src, [box], 0, (0, 0, 255), 1)
-		cv.circle(src, center, 5, (0, 0, 255), -1)
-		print 'Bounding Box Coordinates BL - BR CW:', box
-	
+		# only proceed if the area meets a minimum size
+		# if contours > 10:
+		# 	box = cv.boxPoints(rect)
+		# 	box = np.int0(box)
+		# 	src = cv.drawContours(src, [box], 0, (0, 0, 255), 1)
+		# 	cv.circle(src, center, 5, (0, 0, 255), -1)
+		# 	print box
+
 	print 'Result Frame Per Second:', frame_cnt / (time.time() - start_time)
 
 	# show the frame to our screen
+	cv.imshow("Mask", mask)
 	cv.imshow("Frame", src)
-	char = cv.waitKey(1)
+	char = cv.waitKey(1) & 0xFF
 	if char == 27:
 		break
 	frame_cnt += 1
